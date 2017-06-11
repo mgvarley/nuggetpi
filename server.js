b8f51bd9b8cdbd7f34e1a51689e91404b6a82e7f
Mark Varley <mark@envision-it.co.uk>
2017-06-11 17:26:14 +0200

    Added resin-wifi-connect support

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
-var express = require('express');
-var app = express();
+const express = require('express')
+const app = express()
 
-// reply to request with "Hello World!"
+// Respond with a message when a GET request is made to the homepage
 app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+  res.send('Hello , this is your Nugget')
+})
 
-//start a server on port 80 and log its start to our console
-var server = app.listen(80, function () {
+// Respond with a personalised message when a GET request is made to the homepage
+app.get('/:name', function (req, res) {
+  res.send(`Hello ${req.params.name}, this is your Nugget`)
+})
 
-  var port = server.address().port;
-  console.log('Example app listening on port ', port);
-
+// Start a server on port 80 and log its start to our console
+let server = app.listen(80, function () {
+  let port = server.address().port;
+  console.log(`Example app listening on port ${port}`);
 });
---
+++ start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Start resin-wifi-connect
+export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
+sleep 1
+node resin-wifi-connect/src/app.js --clear=false
+
+# At this point the WiFi connection has been configured and the device has
+# internet - unless the configured WiFi connection is no longer available.
+
+# Start the main application
+npm start
