955f91cc5cc50220c343e4edb705c6551e00cadc
Mark Varley <mark@envision-it.co.uk>
2017-06-11 22:38:29 +0200

    Updated build config and settings

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 
-// Respond with a message when a GET request is made to the homepage
-app.get('/', function (req, res) {
-  res.send('Hello , this is your Nugget')
-})
+// Serve up the UI from the build dir
+app.use('/', express.static(path.join(__dirname, 'build')))
 
 // Respond with a personalised message when a GET request is made to the homepage
-app.get('/:name', function (req, res) {
+/*app.get('/:name', function (req, res) {
   res.send(`Hello ${req.params.name}, this is your Nugget`)
-})
+})*/
 
 // Start a server on port 80 and log its start to our console
-let server = app.listen(80, function () {
+let server = app.listen(8080, function () {
   let port = server.address().port;
   console.log(`Example app listening on port ${port}`);
 });
