b5f1f949e6291d35fc0c7544a770eccada671d3b
Mark Varley <mark@envision-it.co.uk>
2017-06-23 00:26:27 +0200

    Added bluetooth speaker install

--- start.sh
+++ start.sh
@@ -6,5 +6,8 @@ printf "snd_soc_bcm2708\nsnd_soc_bcm2708_i2s\nbcm2708_dmaengine\nsnd_soc_pcm512x
 # re-start gpsd service to pickup new config
 /etc/init.d/gpsd restart
 
+# Install the bluetooth speaker software
+bash <(curl -s https://raw.githubusercontent.com/lukasjapan/bt-speaker/master/install.sh)
+
 # Start the application
 npm start
