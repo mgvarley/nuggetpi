ca18b5c3a4fbdef2e026ad664ac0c2912356b5aa
Mark Varley <mark@envision-it.co.uk>
2017-06-22 23:58:18 +0200

    Setup sound card

--- start.sh
+++ start.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# setup the X400
+printf "snd_soc_bcm2708\nsnd_soc_bcm2708_i2s\nbcm2708_dmaengine\nsnd_soc_pcm512x\nsnd_soc_iqaudio_dac"  >> /etc/modules
+
 # re-start gpsd service to pickup new config
 /etc/init.d/gpsd restart
 
