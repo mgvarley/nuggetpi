dc0fc0a9d42be615a336aa18f0926553a7acf9c7
Mark Varley <mark@envision-it.co.uk>
2017-06-23 10:22:41 +0200

    Updated btspeaker config

--- start.sh
+++ start.sh
@@ -6,8 +6,39 @@ printf "snd_soc_bcm2708\nsnd_soc_bcm2708_i2s\nbcm2708_dmaengine\nsnd_soc_pcm512x
 # re-start gpsd service to pickup new config
 /etc/init.d/gpsd restart
 
-# Install the bluetooth speaker software
-bash <(curl -s https://raw.githubusercontent.com/lukasjapan/bt-speaker/master/install.sh)
+# Add btspeaker user if not exist already
+echo
+echo "Adding btspeaker user..."
+id -u btspeaker &>/dev/null || useradd btspeaker -G audio
+echo "done."
+
+# Download bt-speaker to /opt (or update if already present)
+echo
+cd /opt
+if [ -d bt-speaker ]; then
+  echo "Updating bt-speaker..."
+  cd bt-speaker && git pull
+else
+  echo "Downloading bt-speaker..."
+  git clone https://github.com/lukasjapan/bt-speaker.git
+fi
+echo "done."
+
+# Install and start bt-speaker daemon
+echo
+echo "Registering and starting bt-speaker with systemd..."
+systemctl enable /opt/bt-speaker/bt_speaker.service
+if [ "`systemctl is-active bt_speaker`" != "active" ]; then
+  systemctl start bt_speaker
+else
+  systemctl restart bt_speaker
+fi
+systemctl status bt_speaker
+echo "done."
+
+# Finished
+echo
+echo "BT-Speaker has been installed."
 
 # Start the application
 npm start
